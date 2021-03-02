961be15d25d7d95dca77ef5d89e3aae0dce14aae
Daniel Cabal Fernández <uo19976@uniovi.es>
2021-03-02 20:45:33 +0100

    Añadida cabecera de tipo de contenido a las peticiones

--- src/modules/api-service.js
+++ src/modules/api-service.js
@@ -1,11 +1,15 @@
 import { HOST } from '../constants/api';
 
+const headers = {
+    'Content-Type': 'application/json'
+}
 export default class ApiService {
+
     static async get(endPoint, params) {
         const url = new URL(`${HOST}${endPoint}`);
         url.search = !!params ? new URLSearchParams(params) : undefined;
-
-        const res = await fetch(url);
+        
+        const res = await fetch(url, { headers });
 
         if (res.ok)
             return res.json();
@@ -15,7 +19,7 @@ export default class ApiService {
 
     static async post(endPoint, body) {
         const url = new URL(`${HOST}${endPoint}`);
-        const res = await fetch(url, { method: 'POST', body });
+        const res = await fetch(url, { method: 'POST', body, headers });
 
         if (res.ok)
             return res.json();
