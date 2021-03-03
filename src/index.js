dbad533b121dd552b1dedc9efa6b6ac0a708635e
Daniel Cabal Fernández <uo19976@uniovi.es>
2021-03-03 20:16:31 +0100

    Se envían los datos antes de que el usuario abandone la página

--- src/index.js
+++ src/index.js
@@ -24,19 +24,12 @@ function checkUxaToken(token) {
 }
 
 function initEventListeners() {
-    // window.addEventListener('unload', async _ => {
-    //     pageInfo.setTime();
-    //     await ApiService.post(USER_DATA, mapData(pageInfo.getPageInfo(), tracker.getTrackedData()));
-    // });
+    window.addEventListener('unload', ev => {
+        ev.preventDefault();
+        pageInfo.setTime();
+        ApiService.post(USER_DATA, JSON.stringify(mapData(pageInfo.getPageInfo(), tracker.getTrackedData())));
+    });
     document.addEventListener('mousemove', ev => tracker.trackMouse(ev));
     document.addEventListener('click', ev => tracker.trackClick(ev));
     document.addEventListener('keydown', ev => tracker.trackKeys(ev));
-    const links = document.getElementsByTagName('button');
-    for (const link of links) {
-        link.addEventListener('click', ev => {
-            pageInfo.setTime();
-            console.log(mapData(pageInfo.getPageInfo(), tracker.getTrackedData()))
-            ApiService.post(USER_DATA, JSON.stringify(mapData(pageInfo.getPageInfo(), tracker.getTrackedData())))
-        })
-    }
 }
